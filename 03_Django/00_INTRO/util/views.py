563b515cf6191cd2c4e7b9878d884fa8eda59a71
verba-neo <verba.it.corp@gmail.com>
2023-06-12 17:51:36 +0900

    0612 done

--- 03_Django/00_INTRO/util/views.py
+++ 03_Django/00_INTRO/util/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 
+import random
+import requests
+from bs4 import BeautifulSoup
 
 def lotto(request):
-    
-    return render(request, 'lotto.html')
+    # 로또 번호 6개 뽑기
+    lucky_numbers = random.sample(range(1, 46), 6)
+
+    return render(request, 'lotto.html', {
+        'lucky_numbers': lucky_numbers,
+    })
 
 
 def kospi(request):
+    URL = 'https://finance.naver.com/sise'
+    res = requests.get(URL)
+    html = BeautifulSoup(res.text, 'html.parser')
+    KOSPI = html.select_one('#KOSPI_now').text
     
-    return render(request, 'kospi.html')
+    return render(request, 'kospi.html', {
+        'KOSPI': KOSPI,
+    })
