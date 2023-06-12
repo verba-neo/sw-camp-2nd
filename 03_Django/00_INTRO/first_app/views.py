0773fd934b957f5e4bf765c6de71c0aad7659c34
verba-neo <verba.it.corp@gmail.com>
2023-06-12 11:21:29 +0900

    0612 am

---
+++ 03_Django/00_INTRO/first_app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+
+def hello(request):
+    from django.http import HttpResponse
+    return HttpResponse('Hello Django')
+
+
+def home(request):
+    return render(request, 'home.html')
