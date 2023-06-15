420dbfb8be7c13f2d83eeed0b484c7b79145d715
verba-neo <verba.it.corp@gmail.com>
2023-06-15 16:58:03 +0900

    0615

--- 03_Django/02_MODEL/board/urls.py
+++ 03_Django/02_MODEL/board/urls.py
@@ -13,4 +13,14 @@ urlpatterns = [
     path('', views.index),
     # board/1/ => 단일 게시글 상세
     path('<int:pk>/', views.detail),
+
+    # Update
+    # board/1/edit/ => 게시글 수정용 HTML
+    path('<int:pk>/edit/', views.edit),
+    # board/1/update/ => 게시글 수정 후 저장
+    path('<int:pk>/update/', views.update),
+
+    # Delete
+    # board/1/delete/
+    path('<int:pk>/delete/', views.delete)
 ]
--- 03_Django/02_MODEL/board/views.py
+++ 03_Django/02_MODEL/board/views.py
@@ -1,19 +1,60 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .models import Article
+
+# 게시글 작성용 HTML 제공
+def new(request):    
+    return render(request, 'board/new.html')
 
 
-def new(request):
-    
-    return render()
-
-
+# 게시글 DB에 저장 => redirect
 def create(request):
-    pass
+    article = Article()
+    article.title = request.POST['title']
+    article.content = request.POST['content']
+    article.save()
+    return redirect(f'/board/{article.pk}/')
 
 
+# 전체 게시글 조회
 def index(request):
-    
-    return render()
+    articles = Article.objects.all()
+    return render(request, 'board/index.html', {
+        'articles': articles,
+    })
 
 
+# 단일 게시글 조회
 def detail(request, pk):
-    return render()
+    article = Article.objects.get(pk=pk)
+    return render(request, 'board/detail.html', {
+        'article': article,
+    })
+
+
+# 게시글 수정용 HTML
+def edit(request, pk):
+    article = Article.objects.get(pk=pk)
+    return render(request, 'board/edit.html', {
+        'pk': pk,
+        'article': article,
+    })
+
+
+# 게시글 수정 저장 => redirect
+def update(request, pk):
+    article = Article.objects.get(pk=pk)
+    article.title = request.POST['title']
+    article.content = request.POST['content']
+    article.save()
+    return redirect(f'/board/{article.pk}/')
+
+
+# 단일 게시글 삭제
+def delete(request, pk):
+    if request.method == 'POST':
+        article = Article.objects.get(pk=pk)
+        article.delete()
+    return redirect('/board/')
+
+
